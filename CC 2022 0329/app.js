d7ddbf8a9dc7a0c2e61e0f7ee3414056b250ae1a
Nanaya Miki <nanayamiki@gmail.com>
2022-03-30 10:09:16 -0700

    add a few attempts using forEach for coding challenge 032922

--- CC 2022 0329/app.js
+++ CC 2022 0329/app.js
@@ -4,3 +4,66 @@
 
 // Return true if the array contains the value, false if not.
 
+//P: give an array A and a value x. you want to see if it contains the value. A can have numbers or strings.
+//R: return true if A has x, false if not.
+
+//E: given [1,2,3] as A and 4 as x, should return False
+//   given [7,6,5,] as A and 6 as x, should return True
+//   given [] as A and 5 as x, should return False
+
+//  P: MAke a function that takes in two parameters: an array A and a random number x
+
+//1st attempt
+ function numInArray(arr,num){
+     // look through the arr and see if there are any elements match with num
+     
+     let matchedNum = arr.forEach(element=>element===num?true:false)
+
+    return matchedNum
+
+     // conditional: if they do, return true. else return false.
+ }
+
+ numInArray([1,2,3],4) //should be false
+
+ //2nd attempt
+
+function numInArray(arr,num){
+     // look through the arr and see if there are any elements match with num
+     
+     let matchedNum = arr.forEach(element=>element===num)
+
+     if(arr===num){
+         return true
+     }else{
+         false
+     }
+
+
+     // conditional: if they do, return true. else return false.
+ }
+
+ numInArray([1,2,3,],4) // undefined
+
+
+//3rd attepmt
+
+ function numInArray(arr,num){
+     // look through the arr and see if there are any elements match with num
+     
+     let matchedNum = arr.forEach(element=>element===num)
+
+     if(matchedNum===true){
+         return true
+     }else{
+         false
+     }
+
+
+     // conditional: if they do, return true. else return false.
+ }
+
+ numInArray([1,2,3,],4) // undefined
+
+ //didnt work.. wanted to see if forEach would work but i guess not.. inculdes will work easier anyway
+
