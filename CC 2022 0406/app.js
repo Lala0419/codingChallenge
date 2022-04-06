e5ddd450668f71478da7b67012bde19734e67282
Nanaya Miki <nanayamiki@gmail.com>
2022-04-06 08:01:04 -0700

    Add attempt coding challenge for 04-06-22

--- CC 2022 0406/app.js
+++ CC 2022 0406/app.js
@@ -4,3 +4,27 @@
 // Example:
 // 348597 => [7,9,5,8,4,3]
 // 0 => [0]
+
+//P: Are you ever given an empty array pr undifined? what then? Are you always given digits?
+
+//R: whatever number waas given, it is now split up into an array and the order of the numbers is reversed
+//E: if we were given 9238=> [8,3,2,9]
+//                     330=>[0,3,3]
+//                     undeefined => not a number
+
+//P: make a function that takes in a number 
+function reverseArray(num){
+    //make number a string and split that bebe
+    let newArray= num.toString().split('').reverse()
+
+    //reverse array
+    return Number(newArray)
+
+    //make array integers again
+
+}
+
+reverseArray(9238,[8,3,2,9])//should return [8,3,2,9]
+console.log(reverseArray(330),[0,3,3])//should return [8,3,2,9]
+
+//can't do it this way. have to use map. aghhh
