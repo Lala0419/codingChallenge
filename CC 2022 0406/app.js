682f90b9fe742e2cab4cb27906e92aebdad4a158
Nanaya Miki <nanayamiki@gmail.com>
2022-04-07 08:03:17 -0700

    add solution to the coding challenge for 040622

--- CC 2022 0406/app.js
+++ CC 2022 0406/app.js
@@ -12,9 +12,11 @@
 //                     330=>[0,3,3]
 //                     undeefined => not a number
 
+
+
 //P: make a function that takes in a number 
 function reverseArray(num){
-    //make number a string and split that bebe
+    //make number a string and split that bebe (becasue split wont work wuth numbers)
     let newArray= num.toString().split('').reverse()
 
     //reverse array
@@ -24,18 +26,21 @@ function reverseArray(num){
 
 }
 
+
 reverseArray(9238)//should return [8,3,2,9]
 console.log(reverseArray(330),[0,3,3])//should return [8,3,2,9]
 
 //can't do it this way. have to use map. aghhh
 
 
+
 function reverseArray(num){
-     //make number a string and split that bebe
+     //make number a string and split that bebe (becasue split wont work with numbers)
+     //reverse array
     let numArray= num.toString().split('').reverse()
 
-    //reverse array
     //make array integers again
     return numArray.map(e=>parseInt(e))
-   
 }
+
+//just needed to use map through and make each string element into an integer.
