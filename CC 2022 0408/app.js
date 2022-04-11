5be476fde603055c19a4d4361920df6ede2da25d
Nanaya Miki <nanayamiki@gmail.com>
2022-04-11 09:45:45 -0700

    Add a few attempts and a solution to 040822

--- CC 2022 0408/app.js
+++ CC 2022 0408/app.js
@@ -8,3 +8,55 @@
 // Note:
 // Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
 
+//1st attempt
+
+const myPoint = [6]
+
+function compare(arr){
+    
+    let reduce=arr.reduce((acc,c)=>acc+=c)
+    reduce=reduce/arr.length
+    
+    if(myPoint>reduce){
+        return true
+    }else{
+        return false
+    }
+    
+}
+
+compare([5,5,5,5,5]) //true
+
+//first tryyyyyyy /!!?!?!!!??! No... reduce is the sum of classPoint. you want to make an another variable for the division 
+
+
+
+betterThanAverage([5,5,5,5,5],[6])
+
+//best solution 
+
+function betterThanAverage(classPoints, yourPoints) {
+  return yourPoints > classPoints.reduce((a, b) => a + b, 0) / classPoints.length; 
+}
+
+
+//3rd attempt!!!
+
+function betterThanAverage(classPoints, yourPoints) {
+  // Your code here
+  let sum=classPoints.reduce((acc,c)=>acc+=c)
+    let average=sum/classPoints.length
+    
+    if(yourPoints>average){
+        return true
+    }else{
+        return false
+    }
+
+}
+
+betterThanAverage([5,5,5,5,5],6)
+
+
+
+///lets ggooooooooo
