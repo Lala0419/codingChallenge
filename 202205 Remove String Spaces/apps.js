3b4f25caca47a9af8011c8efedf365827b5382a9
Nanaya Miki <nanayamiki@gmail.com>
2022-05-03 23:55:38 -0700

    added new cc using  array

---
+++ 202205 Remove String Spaces/apps.js
@@ -0,0 +1,18 @@
+// Simple, remove the spaces from the string, then return the resultant string.
+
+//1st 
+
+function noSpace(x){
+  return x.replace(/\s/g, '');
+}
+
+found this solution on google
+
+my way
+
+function noSpace(x){
+    return x.split(' ').join('')
+
+}
+
+console.log(noSpace('8aaaaa dddd r     ')) //8aaaaaddddr
