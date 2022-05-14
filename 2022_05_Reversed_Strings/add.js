d6b8abf4257c876fcbde576c32baddc6ed0d9886
Nanaya Miki <nanayamiki@gmail.com>
2022-05-14 16:12:16 -0700

    added new cc using string

---
+++ 2022_05_Reversed_Strings/add.js
@@ -0,0 +1,15 @@
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+function solution(str){
+  return str.split('').reverse().join('')
+}
+
+
+solution('world') //'dlrow'
+    solution('hello')// 'olleh'
+    solution('') //''
+    solution('h')// 'h'
+
