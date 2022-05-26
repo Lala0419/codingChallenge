bd6876c1fe8e8c06e581d6935e77b23b3f013d85
Nanaya Miki <nanayamiki@gmail.com>
2022-05-26 09:26:48 -0700

    added new cc using array methods

---
+++ 202205_Beginner_reduce_but_grow/apps.js
@@ -0,0 +1,17 @@
+
+// Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+function grow(x){
+    x.reduce((acc,c)=>acc*c)
+
+}
+
+
+grow([1, 2, 3])// 6);
+grow([4, 1, 1, 1, 4]) //16); 
+grow([2, 2, 2, 2, 2, 2])// 64 
+
+
+//first tryyyyyy
