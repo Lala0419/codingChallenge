29d56550d0193ac1969af0d444c2ea83810032ba
Nanaya Miki <nanayamiki@gmail.com>
2022-05-31 12:00:00 -0700

    added new cc using array methods

---
+++ 202206I love you, a little , a lot, passionately ... not at all/app.js
@@ -0,0 +1,42 @@
+// Who remembers back to their time in the schoolyard, when girls would take a flower and tear its petals, saying each of the following phrases each time a petal was torn:
+
+// I love you
+// a little
+// a lot
+// passionately
+// madly
+// not at all
+// When the last petal was torn there were cries of excitement, dreams, surging thoughts and emotions.
+
+// Your goal in this kata is to determine which phrase the girls would say for a flower of a given number of petals, where nb_petals > 0.
+
+function howMuchILoveYou(nbPetals) {
+
+   function howMuchILoveYou(nbPetals) {
+  let arr = ['I love you', 'a little', 'a lot', 'passionately', 'madly', 'not at all'];
+  for(i=0;i<=nbPetals;i++){
+    arr.push(arr[i])
+  }
+  return arr[nbPetals-1]
+}
+
+howMuchILoveYou(8)
+}
+
+
+//best practice
+
+const phrases = [
+    'I love you',
+    'a little',
+    'a lot',
+    'passionately',
+    'madly',
+    'not at all',
+]
+
+function howMuchILoveYou(n) {
+     return phrases[(n - 1) % phrases.length] 
+}
+
+//need to be in Ankiiii
