1c5461921fe88d374d31dfae9eaa40a4f62c89a7
Nanaya Miki <nanayamiki@gmail.com>
2022-07-09 12:00:00 -0700

    added new 7kyu cc usnig array methods

---
+++ 202207_7kyu_no_oddies_here/apps.js
@@ -0,0 +1,14 @@
+// Write a small function that returns the values of an array that are not odd.
+
+// All values in the array will be integers. Return the good values in the order they are given.
+
+
+function noOdds( values ){
+  return values.filter(e=>e%2===0)
+}
+
+
+//easiest 7 kyu probably
+
+noOdds( [0,1] )//, [0] )
+noOdds( [0,1,2,3] )//, [0,2] )
