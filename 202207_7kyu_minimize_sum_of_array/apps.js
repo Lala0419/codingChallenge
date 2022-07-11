c42a748d8c75b7e4dce0348306e673910bbefd6e
Nanaya Miki <nanayamiki@gmail.com>
2022-07-11 12:49:59 -0700

    added desc

--- 202207_7kyu_minimize_sum_of_array/apps.js
+++ 202207_7kyu_minimize_sum_of_array/apps.js
@@ -16,6 +16,6 @@ function minSum(arr) {
 //looked solution, partially understood now but still dont get how sorted[sorted.length - 1 - i] is 
 
 
-minSum({ 5, 4, 2, 3}) //==> return (22) 
-minSum({ 12, 6, 10, 26, 3, 24})// ==> return (342)
-minSum({ 9, 2, 8, 7, 5, 4, 0, 6}) //==> return (74)
+minSum( 5, 4, 2, 3) //==> return (22) 
+minSum( 12, 6, 10, 26, 3, 24)// ==> return (342)
+minSum( 9, 2, 8, 7, 5, 4, 0, 6) //==> return (74)
