81f91f21dc3d5f5061d24561d9e2026b1c4f40c8
Nanaya <nanayamiki@gmail.com>
2023-01-11 22:06:22 -0800

    code challenge from anki 011223

---
+++ 2023 0112 anki/apps.js
@@ -0,0 +1,7 @@
+// Declare a variable and assign it to your fav drink as a string.
+// Make sure there is no whitespace on either side of the string,
+// and print the value to the console
+
+let myfavDrink = "gingerAle";
+myfavDrink = myfavDrink.trim();
+console.log(myfavDrink);
